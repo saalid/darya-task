f4d010a120e7783d59f7223461d339fd71c32247
saalid <amirrezasalari1997@gmail.com>
2025-05-06 16:05:22 +0330

    feat(model): add fileable param in model

--- app/Models/Weather.php
+++ app/Models/Weather.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Weather extends Model
 {
-    //
+
+    protected $fillable = [
+        'latitude',
+        'longitude',
+        'high',
+        'low',
+        'weather',
+        'sunrise',
+        'sunset',
+    ];
 }
